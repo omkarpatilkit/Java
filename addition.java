d77a6b873d167fd4e94dff232bef5f7f6b5537a6
Omkar <omkarpatil3462@gmail.com>
2021-06-27 18:07:53 +0530

    addition

---
+++ addition.java
@@ -0,0 +1,10 @@
+public class addition {
+    public static void main(String[] args) {
+
+        int i = 10;
+        int j = 20;
+
+        System.out.println("Addition is : " + (i + j));
+    }
+
+}
